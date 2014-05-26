f0855c720b159e772f6fa57bb5d2639ab783cafd
Ubuntu <ubuntu@ip-172-31-0-207.ec2.internal>
2014-05-26 05:49:51 +0000

    11

--- notebooks/weather.mapreduce/mr_weather.py
+++ notebooks/weather.mapreduce/mr_weather.py
@@ -3,6 +3,7 @@
 count the number of measurements of each type
 """
 import sys
+import numpy as np
 sys.path.append('/usr/lib/python2.6/dist-packages')
 from mrjob.job import MRJob
 import re
@@ -14,30 +15,21 @@ class MRWeather(MRJob):
         try:
             self.increment_counter('MrJob Counters','mapper-all',1)
             elements=line.split(',')
-            if elements[0]=='station':
-                out=('header',1)
-            else:
-                out=(elements[1],1)
+            number_defined=sum([e!='' for e in elements[3:]])
+            if number_defined==365:
+                if elements[0]!='station':
+                    yield (elements[0],1)
+
         except Exception, e:
             stderr.write('Error in line:\n'+line)
             stderr.write(e)
             self.increment_counter('MrJob Counters','mapper-error',1)
-            out=('error',1)
 
-        finally:
-            yield out
             
-    def combiner(self, word, counts):
-        self.increment_counter('MrJob Counters','combiner',1)
-        yield (word, sum(counts))
-        #l_counts=[c for c in counts]  # extract list from iterator
-        #S=sum(l_counts)
-        #logfile.write('combiner '+word+' ['+','.join([str(c) for c in l_counts])+']='+str(S)+'\n')
-        #yield (word, S)
 
     def reducer(self, word, counts):
         self.increment_counter('MrJob Counters','reducer',1)
-        yield (word, sum(counts))
+        yield (word,sum(counts))
         #l_counts=[c for c in counts]  # extract list from iterator
         #S=sum(l_counts)
         #logfile.write('reducer '+word+' ['+','.join([str(c) for c in l_counts])+']='+str(S)+'\n')
