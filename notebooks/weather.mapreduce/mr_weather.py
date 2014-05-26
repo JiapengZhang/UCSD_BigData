0029da62cf0c4bc3f612b8e96b17ac7aa8beb208
Ubuntu <ubuntu@ip-172-31-0-207.ec2.internal>
2014-05-26 17:54:10 +0000

    22

--- notebooks/weather.mapreduce/mr_weather.py
+++ notebooks/weather.mapreduce/mr_weather.py
@@ -16,9 +16,12 @@ class MRWeather(MRJob):
             self.increment_counter('MrJob Counters','mapper-all',1)
             elements=line.split(',')
             number_defined=sum([e!='' for e in elements[3:]])
-            if number_defined==365:
+            if number_defined==365 and elements[1]=='TMAX':
                 if elements[0]!='station':
-                    yield (elements[0],1)
+                    yield ([elements[0],elements[2]],[1]+elements[:])
+            if number_defined==365 and elements[1]=='TMIN':
+                if elements[0]!='station':
+                    yield ([elements[0],elements[2]],[1]+elements[:])
 
         except Exception, e:
             stderr.write('Error in line:\n'+line)
@@ -29,7 +32,14 @@ class MRWeather(MRJob):
 
     def reducer(self, word, counts):
         self.increment_counter('MrJob Counters','reducer',1)
-        yield (word,sum(counts))
+        new=[]
+        for v in counts:
+            if v[2]=='TMAX':
+                new=v[4:]+new
+            else:
+                new=new+v[4:]
+        if len(new)==730:
+            yield (word[0],[1]+new+[1])
         #l_counts=[c for c in counts]  # extract list from iterator
         #S=sum(l_counts)
         #logfile.write('reducer '+word+' ['+','.join([str(c) for c in l_counts])+']='+str(S)+'\n')
